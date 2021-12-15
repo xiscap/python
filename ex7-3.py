f86b3b8320b45fe0ed7ec7c5ad1fa74fff8214dd
xiscap <93319851+xiscap@users.noreply.github.com>
2021-12-15 10:37:18 +0100

    Create ex7-3.py

---
+++ ex7-3.py
@@ -0,0 +1,15 @@
+"""
+Hay cuatro errores en este código. Identifícalos. (4 pts)
+
+respuesta = input("¿Cuál es el nombre del compañero de Sherlock Holmes?")
+if r = "Watson":
+    print("Correcto!")
+    else
+    print("Incorrecto! Es Watson.")
+"""
+
+res = input("¿Cuál es el nombre del compañero de Sherlock Holmes?")
+if res == "Watson":
+    print("Correcto!")
+else:
+    print("Incorrecto! Es Watson.")
